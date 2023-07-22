74185de07cba83a4036b5d279a488731fc5cc6f7
ay0nn <49310394+ay0nn@users.noreply.github.com>
2023-07-22 19:50:55 +0300

    axios, cors header added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,9 +39,14 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "base.apps.BaseConfig",
     "rest_framework",
+    "corsheaders",
+    
+    
 ]
 
 MIDDLEWARE = [
+     "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -123,3 +128,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CORS_ALLOW_ALL_ORIGINS = True
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,22 @@
-import React from "react";
+
 import{Row, Col} from 'react-bootstrap'
 import Product from "../components/Product";
-import products from "../products";
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function HomeScreen(){
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+
+    async function fetchProducts(){
+
+       const {data} = await axios.get('http://127.0.0.1:8000/api/products/')
+       setProducts(data)
+    }
+    }, [])
+
     return(
         <div>
             <h1>Latest Products</h1>
