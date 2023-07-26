2ff1d340ed2fc0fc5ad073fecb39d42a4b42320b
ay0nn <49310394+ay0nn@users.noreply.github.com>
2023-07-26 23:38:26 +0300

    minor updates in cart functionality

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,20 +1,30 @@
 import axios from "axios";
 import { CART_ADD_ITEM } from "../constants/cartConstants";
 
-export const addtoCart =(id,qty) => async (dispatch, getState)=>{
-    const{data} = await axios.get(`api/products/${id}`)
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  try {
+    const { data } = await axios.get(`/api/products/${id}`);
     dispatch({
-        type: CART_ADD_ITEM,
-        payload:{
-            product: data._id,
-            name: data.name,
-            image: data.image,
-            price: data.price,
-            countInStock: data.countInStock,
-            qty
+      type: CART_ADD_ITEM,
+      payload: {
+        product: data._id,
+        name: data.name,
+        image: data.image,
+        price: data.price,
+        countInStock: data.countInStock,
+        qty,
+      },
+    });
 
-        }
-    })
+    // Use the updated cart items from the action payload
+    const updatedCartItems = getState().cart.cartItems;
 
-    localStorage.setItem('cartItems',JSON.stringify(getState().cart.cartItems))
-}
+    localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
+  } catch (error) {
+    // Handle the error here
+    console.error("Error fetching product:", error);
+    // You can dispatch an error action here to handle the error in your Redux store
+    // For example: dispatch({ type: 'PRODUCT_FETCH_ERROR', error: 'Failed to fetch product.' });
+    // Alternatively, you can show an error message to the user or take other appropriate actions.
+  }
+};
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useParams, useLocation } from 'react-router-dom'; // Update imports
+import { useDispatch, useSelector } from 'react-redux';
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
+import { Message } from '../components/Message';
+import { addToCart } from '../actions/cartActions';
 
 export default function CartScreens() {
+  const dispatch = useDispatch();
+  const { id } = useParams(); // Use useParams to get the parameters
+  const location = useLocation(); // Use useLocation to get the search query
+  const qty = new URLSearchParams(location.search).get('qty');
+  console.log('qty::',qty)
+
+  useEffect(() => {
+    if (id) {
+      dispatch(addToCart(id,qty));
+    }
+  }, [dispatch, id,qty]);
+  
+  
   return (
     <div>
       Cart
     </div>
-  )
+  );
 }
+
+
+
+
