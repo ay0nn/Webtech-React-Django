771b7a3bc19e95530f580177207694ec5679540a
ay0nn <49310394+ay0nn@users.noreply.github.com>
2023-08-05 22:27:06 +0300

    Order page (Backend) from Admin-added

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -88,3 +88,10 @@ def getMyOrders(request):
     orders = user.order_set.all()
     serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getOrders(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
